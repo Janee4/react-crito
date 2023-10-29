b59137a2969f6dd0d131c8335f05158ee8942eac
Jane <Hezha.aban@gmail.com>
2023-10-29 09:23:29 +0100

    inte flyttat upp datumen än

--- src/components/ArticleandnewsBox.js
+++ src/components/ArticleandnewsBox.js
@@ -1,17 +1,25 @@
 import React from 'react'
 
-const ArticleandnewsBox = ({imageSrc, title, description, yellowDate}) => {
+import yellowRectangle from '../assets/images/yellow-rectangle.svg'
+
+const ArticleandnewsBox = ({imageSrc, title, customClass, day, month}) => {
+  const dateClassName = `${month.toLowerCase()}-${day}`;
+  
   return (
     <>
     <div className="text-under-img">
         <a href="how-to.html"><img src={imageSrc} alt="a picture of a woman smiling"/></a>
         <p id="business">Business</p>
         <h4>{title}</h4>
-        <p>{description}</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed hic libero.</p>
       </div>
-      <div className="yellow-rectangle-3">
+      <div className={customClass}>
         <img src={yellowRectangle}alt=""/>
-        <p className={yellowDate}><span className="day">13</span><br/>Mar</p>
+        <p className={dateClassName}>
+          <span className="day">{day}</span>
+          <br />
+          {month}
+        </p>
       </div>
     
     
@@ -21,3 +29,34 @@ const ArticleandnewsBox = ({imageSrc, title, description, yellowDate}) => {
 }
 
 export default ArticleandnewsBox
+
+{/* <div className="text-under-img">
+        <a href="how-to.html"><img src={articleAndNews1} alt="a picture of a woman smiling"/></a>
+        <p id="business">Business</p>
+        <h4>How To Use Digitalization In The Classroom</h4>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed hic libero.</p>
+      </div>
+      <div className="yellow-rectangle-3">
+        <img src={yellowRectangle}alt=""/>
+        <p className="mars-13"><span className="day">13</span><br/>Mar</p>
+      </div>
+      <div className="text-under-img">
+        <a href="how-to.html"><img src={articleAndNews2} alt="a picture of three columns with text inside"/></a>
+        <p id="business">Business</p>
+        <h4>How To Implement Chat GPT In Your Projects</h4>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed hic libero.</p>
+      </div>
+      <div className="yellow-rectangle-2">
+        <img src={yellowRectangle} alt=""/>
+        <p className="mars-17"><span className="day">17</span><br/>Mar</p>
+      </div>
+      <div className="text-under-img">
+        <a href="how-to.html"><img src={articleAndNews3} alt="a picture of a phone lying on top of two books on a table"/></a>
+        <p id="business">Business</p>
+        <h4>The Guide To Support Modern CSS Design</h4>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed hic libero.</p>
+      </div>
+      <div className= "yellow-rectangle-1">
+        <img src={yellowRectangle} alt=""/>
+        <p className="mars-25">25<br/>Mar</p>
+      </div> */}
--- src/components/ArticleandnewsSection.js
+++ src/components/ArticleandnewsSection.js
@@ -4,6 +4,7 @@ import articleAndNews1 from '../assets/images/articles-and-news-1.png'
 import articleAndNews2 from '../assets/images/articles-and-news-2.png'
 import articleAndNews3 from '../assets/images/articles-and-news-3.png'
 import yellowRectangle from '../assets/images/yellow-rectangle.svg'
+import ArticleandnewsBox from './ArticleandnewsBox'
 
 
 const ArticleandnewsSection = () => {
@@ -20,37 +21,10 @@ const ArticleandnewsSection = () => {
       </div>
     </div>
     <div className="how-to-section">
-    <div className="text-under-img">
-        <a href="how-to.html"><img src={articleAndNews1} alt="a picture of a woman smiling"/></a>
-        <p id="business">Business</p>
-        <h4>How To Use Digitalization In The Classroom</h4>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed hic libero.</p>
+    <ArticleandnewsBox title="How To Use Digitalization In The Classroom" imageSrc={articleAndNews1} customClass="yellow-rectangle-3"  day="13" month="Mar" />
+    <ArticleandnewsBox title="How To Implement Chat GPT In Your Projects"  imageSrc={articleAndNews2} customClass="yellow-rectangle-2"  day="17" month="Mar" />
+    <ArticleandnewsBox title="The Guide To Support Modern CSS Design" imageSrc={articleAndNews3} customClass="yellow-rectangle-1"  day="25" month="Mar"/>
       </div>
-      <div className="yellow-rectangle-3">
-        <img src={yellowRectangle}alt=""/>
-        <p className="mars-13"><span className="day">13</span><br/>Mar</p>
-      </div>
-      <div className="text-under-img">
-        <a href="how-to.html"><img src={articleAndNews2} alt="a picture of three columns with text inside"/></a>
-        <p id="business">Business</p>
-        <h4>How To Implement Chat GPT In Your Projects</h4>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed hic libero.</p>
-      </div>
-      <div className="yellow-rectangle-2">
-        <img src={yellowRectangle} alt=""/>
-        <p className="mars-17"><span className="day">17</span><br/>Mar</p>
-      </div>
-      <div className="text-under-img">
-        <a href="how-to.html"><img src={articleAndNews3} alt="a picture of a phone lying on top of two books on a table"/></a>
-        <p id="business">Business</p>
-        <h4>The Guide To Support Modern CSS Design</h4>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed hic libero.</p>
-      </div>
-      <div className= "yellow-rectangle-1">
-        <img src={yellowRectangle} alt=""/>
-        <p className="mars-25">25<br/>Mar</p>
-      </div>
-    </div>
     <div className="circles">
       <div className="circle-1"></div>
       <div className="circle"></div>
@@ -59,6 +33,7 @@ const ArticleandnewsSection = () => {
       <div className="circle"></div>
     </div>
     </div>
+    
     </section>
   )
 }
