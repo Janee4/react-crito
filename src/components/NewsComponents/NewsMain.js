c8549ae0344790b0d390c21572efb42da20a0a40
Jane <hezha.aban@gmail.com>
2023-10-29 21:17:20 +0100

    test

--- src/components/NewsComponents/NewsMain.js
+++ src/components/NewsComponents/NewsMain.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import ArticleandnewsBox from '../ArticleandnewsBox'
 
 const NewsMain = () => {
   return (
     <div className= "OurNewsnArticles">
     
-
+<ArticleandnewsBox imageSrc="" title="" customClass="" dateClassName="" day="" month="" />
 
 
 
