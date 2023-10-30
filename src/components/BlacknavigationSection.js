8f722406c4c5135c0e7a1c6e529aa9027f4322c3
Jane <hezha.aban@gmail.com>
2023-10-30 12:46:01 +0100

    blacknavigation seciton klar på homesidan också

--- src/components/BlacknavigationSection.js
+++ src/components/BlacknavigationSection.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import BlacknavigationNews from './NewsComponents/BlacknavigationNews'
 
 import blackCritoLogo from '../assets/images/black-crito-logo.svg'
 import greyLines from '../assets/images/grey-lines.svg'
@@ -11,34 +12,10 @@ const BlacknavigationSection = () => {
       <img src={blackCritoLogo} alt=""/>
       <p>Lorem ipsum dolor sit amet consectetur<br/> adipisicing elit. Placeat obcaecati voluptas<br/> voluptates! Voluptates laborum nam<br/> ratione minus necessitatibus, iure<br/> praesentium.</p>
     </div>
-    <div className="small-titles">
-      <h4 className="company">Company</h4>
-      <a href="about.html">About</a>
-      <a href="features.html">Features</a>
-      <a href="works.html">Works</a>
-      <a href="career.html">Career</a>
-    </div>
-    <div className="small-titles help">
-      <h4 className="help">Help</h4>
-      <a href="customersupport.html">Customer Support</a>
-      <a href="deliverydetails.html">Delivery Details</a>
-      <a href="terms-conditions.html">Terms & Conditions</a>
-      <a href="privacypolicy.html">Privacy Policy</a>
-    </div>
-    <div className="small-titles">
-      <h4>Resources</h4>
-      <a href="ebooks.html">Free eBooks</a>
-      <a href="dev-tutorial.html">Development Tutorial</a>
-      <a href="blog.html">How to -Blog</a>
-      <a href="youtube.html">Youtube Playlist</a>
-    </div>
-    <div className="small-titles link">
-      <h4>Link</h4>
-      <a href="ebooks.html">Free eBooks</a>
-      <a href="dev-tutorial.html">Development Tutorial</a>
-      <a href="blog.html">How to -Blog</a>
-      <a href="youtube.html">Youtube Playlist</a>
-    </div>
+    <BlacknavigationNews divName="small-titles" titleClass="company" title="Company" title1="About" title2="Features" title3="Works" title4="Career" />
+    <BlacknavigationNews divName="small-titles help" titleClass="help" title="Help" title1="Customer Support" title2="Delivery Details" title3="Terms & Conditions" title4="Privacy Policy" />
+    <BlacknavigationNews divName="small-titles" title="Resources" title1="Free eBooks" title2="Development Tutorial" title3="How to -Blog" title4="Youtube Playlist" />
+    <BlacknavigationNews divName="small-titles link" title="Link" title1="Free eBooks" title2="Development Tutorial" title3="How to -Blog" title4="Youtube Playlist"/>
   </div>
   <div className="grey-lines">
     <img src={greyLines} alt=""/>
