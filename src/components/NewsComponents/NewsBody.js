3282ba2219e948a357008b3fe7e94add4b33ba09
Jane <hezha.aban@gmail.com>
2023-10-30 12:40:43 +0100

    klar med black navigation

--- src/components/NewsComponents/NewsBody.js
+++ src/components/NewsComponents/NewsBody.js
@@ -70,9 +70,9 @@ const NewsBody = () => {
       <p>Lorem ipsum dolor sit amet consectetur<br/> adipisicing elit. Placeat obcaecati voluptas<br/> voluptates! Voluptates laborum nam<br/> ratione minus necessitatibus, iure<br/> praesentium.</p>
     </div>
     <BlacknavigationNews divName="small-titles" titleClass="company" title="Company" title1="About" title2="Features" title3="Works" title4="Career" />
-    <BlacknavigationNews divName="small-titles help" titleClass="help" title="Help" title1="" title2="" title3="" title4="" />
-    <BlacknavigationNews divName="small-titles" title="Resources" title1="" title2="" title3="" title4="" />
-    <BlacknavigationNews divName="small-titles link" title="Link" title1="" title2="" title3="" title4=""/>
+    <BlacknavigationNews divName="small-titles help" titleClass="help" title="Help" title1="Customer Support" title2="Delivery Details" title3="Terms & Conditions" title4="Privacy Policy" />
+    <BlacknavigationNews divName="small-titles" title="Resources" title1="Free eBooks" title2="Development Tutorial" title3="How to -Blog" title4="Youtube Playlist" />
+    <BlacknavigationNews divName="small-titles link" title="Link" title1="Free eBooks" title2="Development Tutorial" title3="How to -Blog" title4="Youtube Playlist"/>
   </div>
   <div className="grey-lines">
     <img src={greyLines} alt=""/>
