84b57530f818908c9df736bf1ce844e138deb238
Jane <Hezha.aban@gmail.com>
2023-11-01 21:06:06 +0100

    left side klar

--- src/components/ServiceComponents.js/ServicesBody.js
+++ src/components/ServiceComponents.js/ServicesBody.js
@@ -14,14 +14,23 @@ const ServicesBody = () => {
           <InfoParagraph description="Kimberly Hansen" showYellowCircle={false}/>
         </div>
         <img src={kimberlyHansenBigPic}/>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, <br/> magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
-        </p>
-        <p>
-        Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. <br/>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra<br/> nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. uspendisse<br/> dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. <br/> Ut nonummy.
+        <div className="paragraph-div">
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, <br/> magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
+          </p>
+          <p>
+            Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. <br/>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra<br/> nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. uspendisse<br/> dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. <br/> Ut nonummy.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,<br/> magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. unc <br/> viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et <br/> malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
+          </p>
+          <p>
+            Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.<br/>
+            Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem <br/>venenatis eleifend. Ut nonummy.
+          </p>
 
-        </p>
-        <p></p>
-        <p></p>
+        </div>
+        
       
 
       </div>
