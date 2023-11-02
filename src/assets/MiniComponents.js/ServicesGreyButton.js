a140fdf0f0ff7e36e6e1f84b6443b707ed837e7e
Jane <hezha.aban@gmail.com>
2023-11-02 13:22:13 +0100

    grey button

---
+++ src/assets/MiniComponents.js/ServicesGreyButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServicesGreyButton = () => {
+  return (
+    <div>ServicesGreyButton</div>
+  )
+}
+
+export default ServicesGreyButton
--- src/components/ServiceComponents.js/ServicesBody.js
+++ src/components/ServiceComponents.js/ServicesBody.js
@@ -2,6 +2,7 @@ import React from 'react'
 import InfoParagraph from './InfoParagraph'
 import kimberlyHansenBigPic from '../../assets/images/Kimberly-Hansen-Big-Pic.png'
 import quoteSign from '../../assets/images/quote-sign.svg'
+import ServicesGreyButton from '../../assets/MiniComponents.js/ServicesGreyButton'
 
 const ServicesBody = () => {
   return (
@@ -27,7 +28,7 @@ const ServicesBody = () => {
           </p>
           <p>
             Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.<br/>
-            Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem <br/>venenatis eleifend. Ut nonummy.
+            Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at <br/>sem venenatis eleifend. Ut nonummy.
           </p>
         </div>
         
@@ -39,9 +40,10 @@ const ServicesBody = () => {
         </div>
 
         <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. unc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce<br/> posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros<br/> quis urna. unc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi<br/> tristique senectus et netus et malesuada fames ac turpis egestas. 
         </p>
       
+        <ServicesGreyButton />
 
       </div>
       
