6fd139578d6f1a65040f8216b2afd7201ce9296a
Jane <hezha.aban@gmail.com>
2023-11-03 22:06:20 +0100

    fixat recent posts o categories

--- src/components/ServiceComponents.js/Categoriesbox.js
+++ src/components/ServiceComponents.js/Categoriesbox.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Categoriesbox = ({firstText, secondText}) => {
   return (
     <p>
-      <span className="first-text">{firstText}</span> -{' '}
+      <span className="first-text">{firstText}</span> - {'   '}
       <span className="second-text">{secondText}</span>
     </p>
   )
